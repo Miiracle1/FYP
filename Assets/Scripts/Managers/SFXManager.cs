2edcc532297e12ebc302aa28a71340f6b9f218e5
Miiracle1 <honjing8612@gmail.com>
2026-06-19 19:16:01 +0800

    Update SFXManager.cs

--- Assets/Scripts/Managers/SFXManager.cs
+++ Assets/Scripts/Managers/SFXManager.cs
@@ -15,6 +15,12 @@ public class SFXManager : MonoBehaviour
             instance = this;
     }
 
+    /// <summary>
+    /// Spawns prefab to play an sfx one shot, then it despawns.
+    /// </summary>
+    /// <param name="clip"></param>
+    /// <param name="spawnTransform"></param>
+    /// <param name="volume"></param>
     public void PlaySfxClip(AudioClip clip, Transform spawnTransform, float volume)
     {
         var audioSource = Instantiate(sfxPrefab, spawnTransform.position, Quaternion.identity);
