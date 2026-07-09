15f344d48dfcd7266036b5d5567df2f834261381
Miiracle1 <honjing8612@gmail.com>
2026-07-09 21:15:56 +0800

    win trigger
    spider phase 2 logic (WIP

---
+++ Assets/Scripts/Spider/BT/DisableAgent.cs
@@ -0,0 +1,41 @@
+using BehaviorDesigner.Runtime.Tasks;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Spider.BT
+{
+    [TaskCategory("Spider")]
+    [TaskDescription("Stops agent movement or nav mesh behavior")]
+    public class DisableAgent : Action
+    {
+        private SpiderAI spider;
+        private NavMeshAgent agent;
+
+        public bool grabState; //Determine if task is for grab or attach
+
+        public override void OnStart()
+        {
+            spider = GetComponent<SpiderAI>();
+            agent = GetComponent<NavMeshAgent>();
+
+            StopAgent();
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            return TaskStatus.Success;
+        }
+
+        private void StopAgent()
+        { 
+            agent.isStopped = true;
+            agent.path = null;
+
+            if (grabState && spider.IsGrabbed == false)
+                spider.SetGrabbed(true);
+
+            if (!grabState && spider.IsAttached == false)
+                spider.SetAttached(true);
+        }
+    }
+}
